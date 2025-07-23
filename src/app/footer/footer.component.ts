8fa0116f399e2a9b5525dc4d17e2ce8361016065
DeniBademi <denis.zaharievv@gmail.com>
2025-07-23 13:16:45 +0300

    locale aware footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LOCALE_ID } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  public locale = inject(LOCALE_ID);
 }
