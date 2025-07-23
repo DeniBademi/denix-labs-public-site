076280122bac6c5994b1f55561a7730ed575df12
DeniBademi <denis.zaharievv@gmail.com>
2025-07-23 13:59:40 +0300

    dsada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { SolutionsComponent } from './pages/solutions/solutions.component';
-import { CookiePolicyComponent } from './pages/legal/cookie-policy/cookie-policy.component';
-import { PrivacyPolicyComponent } from './pages/legal/privacy-policy/privacy-policy.component';
-import { TermsOfUseComponent } from './pages/legal/terms-of-use/terms-of-use.component';
-import { AutomaticCustomerSupportComponent } from './pages/solutions/automatic-customer-support/automatic-customer-support.component';
-import { DocumentAnalysisComponent } from './pages/solutions/document-analysis/document-analysis.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { InventoryManagementComponent } from './pages/solutions/inventory-management/inventory-management.component';
-import { PredictiveMaintenanceComponent } from './pages/solutions/predictive-maintenance/predictive-maintenance.component';
-import { QaAndAnomaliesComponent } from './pages/solutions/qa-and-anomalies/qa-and-anomalies.component';
-import { ReportWritingComponent } from './pages/solutions/report-writing/report-writing.component';
-import { FaceRecognitionComponent } from './pages/solutions/face-recognition/face-recognition.component';
-import { RiskAssessmentComponent } from './pages/solutions/risk-assessment/risk-assessment.component';
-import { ArticleComponent } from './pages/article/article.component';
-import { BlogComponent } from './pages/blog/blog.component';
-import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
@@ -41,5 +21,5 @@ export const routes: Routes = [
     { path: 'privacy', loadComponent: () => import('./pages/legal/privacy-policy/privacy-policy.component').then(m => m.PrivacyPolicyComponent) },
     { path: 'terms', loadComponent: () => import('./pages/legal/terms-of-use/terms-of-use.component').then(m => m.TermsOfUseComponent) },
     { path: 'not-found', loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent) },
-    // { path: '**', redirectTo: 'not-found' }, // Redirect any unmatched route to login
+    { path: '**', redirectTo: 'not-found' }, // Redirect any unmatched route to login
 ];
